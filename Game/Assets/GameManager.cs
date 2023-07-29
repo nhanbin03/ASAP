272e9f034e822de95615b967774c78d62d467c21
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-29 15:05:38 +0700

    Create game state manger

---
+++ Game/Assets/GameManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake() {
+        Instance = this;
+    }
+
+    public void UpdateGameState(GameState newState) {
+        State = newState;
+
+        switch (newState) {
+            case GameState.SelectDifficulty:
+                break;
+            case GameState.Gameplay:
+                break;
+            case GameState.Victory:
+                break;
+            case GameState.Lose:
+                break;
+        }
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    
+}
+
+public enum GameState {
+    SelectDifficulty,
+    Gameplay,
+    Victory,
+    Lose
+}
